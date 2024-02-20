5c6b965c1128e30642198e6329c5d3e1d9f8f3bf
Ilham Fitri Ardiansyah <ardiansyahilham11@gmail.com>
2024-02-20 18:12:14 +0700

    [FIX]

--- src/main/java/com/enigma/enrollment_java/Main.java
+++ src/main/java/com/enigma/enrollment_java/Main.java
@@ -1,4 +1,18 @@
 package com.enigma.enrollment_java;
 
+import com.enigma.enrollment_java.entity.Course;
+import com.enigma.enrollment_java.repository.CourseRepository;
+import com.enigma.enrollment_java.repository.impl.CourseRepositoryImpl;
+import com.enigma.enrollment_java.util.JpaUtil;
+import jakarta.persistence.EntityManager;
+
 public class Main {
+    public static void main(String[] args) {
+        EntityManager entityManager = JpaUtil.getEntityManager();
+
+        CourseRepository courseRepository = new CourseRepositoryImpl(entityManager);
+
+//        courseRepository.delete(5);
+        courseRepository.update(4,"Kalkulus II");
+    }
 }
