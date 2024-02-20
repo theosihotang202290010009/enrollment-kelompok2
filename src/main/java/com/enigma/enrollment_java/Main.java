a53151f237ba914d44a005db2d56e9829eeeeee3
Saiful Rahman <sr.perante@gmail.com>
2024-02-20 20:39:50 +0700

    cenceled readAll

--- src/main/java/com/enigma/enrollment_java/Main.java
+++ src/main/java/com/enigma/enrollment_java/Main.java
@@ -3,13 +3,10 @@ package com.enigma.enrollment_java;
 import com.enigma.enrollment_java.dto.request.EnrollDetailRequest;
 import com.enigma.enrollment_java.dto.request.EnrollRequest;
 import com.enigma.enrollment_java.entity.Course;
-import com.enigma.enrollment_java.entity.Enroll;
 import com.enigma.enrollment_java.entity.Period;
-import com.enigma.enrollment_java.repository.CourseRepository;
 import com.enigma.enrollment_java.repository.EnrollRepository;
 import com.enigma.enrollment_java.repository.PeriodRepository;
 import com.enigma.enrollment_java.repository.StudentRepository;
-import com.enigma.enrollment_java.repository.impl.CourseRepositoryImpl;
 import com.enigma.enrollment_java.repository.impl.EnrollRepositoryImpl;
 import com.enigma.enrollment_java.repository.impl.PeriodRepositoryImpl;
 import com.enigma.enrollment_java.repository.impl.StudentRepositoryImpl;
@@ -22,37 +19,42 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         EntityManager entityManager = JpaUtil.getEntityManager();
+        // addEnrollDetail(entityManager);
+
+        entityManager.close();
+        JpaUtil.shutdown();
+    }
+
+    private static void addEnrollDetail(EntityManager entityManager) {
         EnrollRepository enrollRepository = new EnrollRepositoryImpl(entityManager);
         Course course = entityManager.find(Course.class, 1);
-        Period period =entityManager.find(Period.class, 1);
+        Period period = entityManager.find(Period.class, 1);
 
         Course course1 = entityManager.find(Course.class, 3);
-        Period period1 =entityManager.find(Period.class, 1);
+        Period period1 = entityManager.find(Period.class, 1);
 
         Course course2 = entityManager.find(Course.class, 3);
-        Period period2 =entityManager.find(Period.class, 3);
+        Period period2 = entityManager.find(Period.class, 3);
 
         Course course3 = entityManager.find(Course.class, 2);
-        Period period3 =entityManager.find(Period.class, 2);
+        Period period3 = entityManager.find(Period.class, 2);
 
         Course course4 = entityManager.find(Course.class, 4);
-        Period period4 =entityManager.find(Period.class, 2);
+        Period period4 = entityManager.find(Period.class, 2);
 
 
-        EnrollRequest enrollRequest =new EnrollRequest("Ucogg","Sastra Mesin");
+        EnrollRequest enrollRequest = new EnrollRequest("Ucogg", "Sastra Mesin");
         enrollRequest.setEnrollDetailRequests(
                 List.of(
-                        new EnrollDetailRequest(1,1),
-                        new EnrollDetailRequest(3,1),
-                        new EnrollDetailRequest(3,3),
-                        new EnrollDetailRequest(2,2),
-                        new EnrollDetailRequest(4,2)
+                        new EnrollDetailRequest(1, 1),
+                        new EnrollDetailRequest(3, 1),
+                        new EnrollDetailRequest(3, 3),
+                        new EnrollDetailRequest(2, 2),
+                        new EnrollDetailRequest(4, 2)
                 )
         );
 
         enrollRepository.save(enrollRequest);
-        entityManager.close();
-        JpaUtil.shutdown();
     }
 
     private static void addStudent(EntityManager entityManager) {
--- src/main/java/com/enigma/enrollment_java/repository/EnrollRepository.java
+++ src/main/java/com/enigma/enrollment_java/repository/EnrollRepository.java
@@ -1,8 +1,11 @@
 package com.enigma.enrollment_java.repository;
 
 import com.enigma.enrollment_java.dto.request.EnrollRequest;
-import com.enigma.enrollment_java.entity.Enroll;
+
+import java.util.List;
 
 public interface EnrollRepository {
     void save(EnrollRequest enrollRequest);
+
+
 }
