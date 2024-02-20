d44b6b25709fd5d5f84df4eb2c8919e23c16d8eb
Theo <theosihotang98@gmail.com>
2024-02-20 16:57:17 +0700

    [FEATURE] add details

--- src/main/java/com/enigma/enrollment_java/repository/impl/EnrollRepositoryImpl.java
+++ src/main/java/com/enigma/enrollment_java/repository/impl/EnrollRepositoryImpl.java
@@ -1,11 +1,16 @@
 package com.enigma.enrollment_java.repository.impl;
 
+import com.enigma.enrollment_java.dto.request.EnrollDetailRequest;
 import com.enigma.enrollment_java.dto.request.EnrollRequest;
 import com.enigma.enrollment_java.entity.Enroll;
+import com.enigma.enrollment_java.entity.EnrollDetail;
 import com.enigma.enrollment_java.entity.Student;
 import com.enigma.enrollment_java.repository.EnrollRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EnrollRepositoryImpl implements EnrollRepository {
     private final EntityManager entityManager;
@@ -20,7 +25,19 @@ public class EnrollRepositoryImpl implements EnrollRepository {
         transaction.begin();
         Enroll enroll = new Enroll();
         enroll.setStudentId(new Student(enrollRequest.getName(), enrollRequest.getMajor()));
+
+        List<EnrollDetail> enrollDetails = new ArrayList<>();
+
+        for (EnrollDetailRequest enrollDetailRequest : enrollRequest.getEnrollDetailRequests()) {
+            EnrollDetail enrollDetail = new EnrollDetail();
+            enrollDetail.setEnrollId(enrollDetailRequest.getEnrollId());
+            enrollDetail.setCourseId(enrollDetailRequest.getCourseId());
+            enrollDetail.setPeriodId(enrollDetailRequest.getPeriod());
+            enrollDetails.add(enrollDetail);
+        }
+        enroll.setEnrollDetails(enrollDetails);
         entityManager.persist(enroll);
+
         transaction.commit();
         entityManager.close();
     }
